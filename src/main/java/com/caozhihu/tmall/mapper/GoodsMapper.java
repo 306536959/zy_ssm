81e2d75095fbd4c94cbefc70a3dd92cb96b41ea1
xa <306536959@qq.com>
2024-04-22 20:21:48 +0800

    修正

--- src/main/java/com/caozhihu/tmall/mapper/GoodsMapper.java
+++ src/main/java/com/caozhihu/tmall/mapper/GoodsMapper.java
@@ -13,28 +13,28 @@ public interface GoodsMapper {
     @Select("select * from goodsinfo")
     List<Goods> list();
     @Select("select * from goodsinfo where name like concat('%', #{name}, '%')")
-    List<Goods> search(String name,BigDecimal price,String type);
+    List<Goods> search(@Param("name") String name);
     @Select("select * from goodsinfo where name like concat('%', #{name}, '%')")
-    List<Goods> searchByName(String name);
+    List<Goods> searchByName(@Param("name") String name);
 
     @Select("select * from goodsinfo where name like concat('%', #{name}, '%') and price = #{price}")
-    List<Goods> searchByNameAndPrice(String name, BigDecimal price);
+    List<Goods> searchByNameAndPrice(@Param("name") String name, @Param("price") BigDecimal price);
 
     @Select("select * from goodsinfo where name like concat('%', #{name}, '%') and price = #{price} and type = #{type}")
     List<Goods> searchByNameAndPriceAndType(@Param("name") String name, @Param("price") BigDecimal price, @Param("type") String type);
 
     @Select("select * from goodsinfo where type = #{type}")
-    List<Goods> searchByType(String type);
+    List<Goods> searchByType(@Param("type") String type);
     @Select("select * from goodsinfo where price = #{price}")
-    List<Goods> searchByPrice(BigDecimal price);
+    List<Goods> searchByPrice(@Param("price") BigDecimal price);
     @Select("select * from goodsinfo where price = #{price} and type = #{type}")
-    List<Goods> searchByPriceAndType(BigDecimal price, String type);
+    List<Goods> searchByPriceAndType(@Param("price") BigDecimal price,@Param("type") String type);
     @Select("select * from goodsinfo where name like concat('%', #{name}, '%') and type = #{type}")
-    List<Goods> searchByNameAndType(String name, String type);
+    List<Goods> searchByNameAndType(@Param("name") String name,@Param("type") String type);
     @Insert("insert into goodsinfo(name, price, num, type,time) values(#{name}, #{price},#{num}, #{type},#{time} )")
     void insert(Goods goods);
     @Select("select * from goodsinfo where name = #{name}")
-    Goods selectByName(String name);
+    Goods selectByName(@Param("name") String name);
     @Update("update goodsinfo set oldtime =#{oldtime},price = #{price}, num = #{num}, type = #{type}, time = #{time} Where name = #{name}")
     void updata(Goods goods);
 }
